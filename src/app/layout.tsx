f8482b335060a9e3cf308aea4e9e2d9b75e9c48f
SamsonGulma <sams0ngultek@gmail.com>
2025-03-12 21:56:52 +0000

    Mar 13, 2025, 12:56 AM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,12 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
+  variable: "--font-geist-sans, --font-sans",
   subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+  variable: "--font-geist-mono, --font-sans",
   subsets: ["latin"],
 });
 
