d76ac2ee12acc80230320cace96ec07af643c115
SamsonGulma <sams0ngultek@gmail.com>
2025-03-12 22:07:26 +0000

    Mar 13, 2025, 1:07 AM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "E-commerce MVP",
-  description: "Nothing to say for now",
+  description: "Nothing to say",
 };
 
 export default function RootLayout({
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background min-h-screen font-sans`}
       >
         {children}
       </body>
