16e565289d600cabd855e2f707c906abba64d12e
SamsonGulma <sams0ngultek@gmail.com>
2025-03-18 15:51:49 +0300

    adding components for a style main layout.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,10 @@
-export default function AdminLayout({ children,
-    
-}: Readonly<{ children: React.ReactNode }>) {
+export default function AdminLayout({ children, }: Readonly<
+    { children: React.ReactNode }>) {
     return <>
-        
+        <Nav>
+
+        </Nav>
+
+        <div className="container my-6">{children}</div>
     </>
 }
